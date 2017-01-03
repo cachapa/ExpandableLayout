9884dcd5e433642c61802629f42cfb4a7905c946
Daniel Cachapa <cachapa@gmail.com>
2017-01-03 21:15:13 +0000

    Improve horizontal layout demo
    Use icon for button
    Rotate icon according to expansion progress

--- demo/src/main/java/net/cachapa/expandablelayoutdemo/HorizontalFragment.java
+++ demo/src/main/java/net/cachapa/expandablelayoutdemo/HorizontalFragment.java
@@ -6,12 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import net.cachapa.expandablelayout.ExpandableLayout;
 
-public class HorizontalFragment extends Fragment implements View.OnClickListener {
-
+public class HorizontalFragment extends Fragment implements View.OnClickListener, ExpandableLayout.OnExpansionUpdateListener {
     private ExpandableLayout expandableLayout;
+    private ImageView expandButton;
 
     @Nullable
     @Override
@@ -19,9 +20,17 @@ public class HorizontalFragment extends Fragment implements View.OnClickListener
         View rootView = inflater.inflate(R.layout.horizontal_fragment, container, false);
 
         expandableLayout = (ExpandableLayout) rootView.findViewById(R.id.expandable_layout);
-        rootView.findViewById(R.id.expand_button).setOnClickListener(this);
+        expandButton = (ImageView) rootView.findViewById(R.id.expand_button);
+
+        expandableLayout.setOnExpansionUpdateListener(this);
+        expandButton.setOnClickListener(this);
 
         return rootView;
+    }
+
+    @Override
+    public void onExpansionUpdate(float expansionFraction) {
+        expandButton.setRotation(expansionFraction * 180);
     }
 
     @Override
