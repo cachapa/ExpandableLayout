e8b5e6c1c7165398abd6ef71f9bf3b7e5eae062f
DanChaltiel <dan.chaltiel@gmail.com>
2017-11-22 09:57:11 +0100

    Update RecyclerViewFragment.java

--- demo/src/main/java/net/cachapa/expandablelayoutdemo/RecyclerViewFragment.java
+++ demo/src/main/java/net/cachapa/expandablelayoutdemo/RecyclerViewFragment.java
@@ -99,7 +99,7 @@ public class RecyclerViewFragment extends Fragment {
             @Override
             public void onExpansionUpdate(float expansionFraction, int state) {
                 Log.d("ExpandableLayout", "State: " + state);
-                if (state!=ExpandableLayout.State.COLLAPSED) {
+                if (state != ExpandableLayout.State.COLLAPSED) {
                     recyclerView.smoothScrollToPosition(getAdapterPosition());
                 }
             }
